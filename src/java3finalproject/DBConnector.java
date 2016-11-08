8b733d2641d1696f910dd98768be1334f8741f48
tanona <billtinvt@gmail.com>
2016-11-07 19:32:03 -0500

    Commit changes to DBConnector just to make git happy before git stash apply

--- src/java3finalproject/DBConnector.java
+++ src/java3finalproject/DBConnector.java
@@ -13,163 +13,142 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/** 
+/**
  * @Course: SDEV 450 ~ Enterprise Java Programming
  * @Contributors: Charlotte Hirschberger, Wayne Riley
  * @Created Date: October 16, 2016
  * @Last update: October 28, 2016
- * @Description: This class is responsible for initiating and returning the 
- *      connection to a database and executing queries received in String parameters.
+ * @Description: This class is responsible for initiating and returning the
+ * connection to a database and executing queries received in String parameters.
  */
-
-/**Start Charlotte's code**/
+/**
+ * Start Charlotte's code*
+ */
 public class DBConnector {
+
     private static Connection conn;
-    private static String dbUser = "champlain";
-    private static String dbPassword = "Fin8lPr0ject";
+  //  private static String dbUser = "champlain";
+  //  private static String dbPassword = "Fin8lPr0ject";
+    private static String dbUser = "root";
+    private static String dbPassword = "badt00th";
     private static String dbPath = "jdbc:mysql://localhost/projectdb";
-    
+
     /**
      * Constructor automatically attempts to establish connection using database
      * credentials from member variables
      */
-    public DBConnector()
-    {
-        try
-        {
+    public DBConnector() {
+        try {
             conn = DriverManager.getConnection(dbPath, dbUser, dbPassword);
+        } catch (Exception ex) {
+            System.out.println("Error connecting to database."
+                    + "Error message: " + ex.getMessage());
         }
-         catch(Exception ex)
-         {
-             System.out.println("Error connecting to database."
-                     + "Error message: " + ex.getMessage());
-         }
     }
-        
-        public Connection getConnection()
-    {
+
+    public Connection getConnection() {
         return conn;
     }
-    /**
-     * General method for executing SQL queries, for use with SELECT statements     * 
-     * @param query SQL statement in a string
-     * @return 
-     */
-    public List<Map<String, Object>> retrieveRecords(String query)
-    {
-       List<Map<String, Object>> results = new ArrayList<>();
-       HashMap<String, Object> row;
 
-       try (Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query))
-       {       
-           ResultSetMetaData metaData = rs.getMetaData();
-           int colCount = metaData.getColumnCount();
-            while(rs.next())
-            {
+    /**
+     * General method for executing SQL queries, for use with SELECT statements
+     *
+     *
+     * @param query SQL statement in a string
+     * @return
+     */
+    public List<Map<String, Object>> retrieveRecords(String query) {
+        List<Map<String, Object>> results = new ArrayList<>();
+        HashMap<String, Object> row;
+
+        try (Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(query)) {
+            ResultSetMetaData metaData = rs.getMetaData();
+            int colCount = metaData.getColumnCount();
+            while (rs.next()) {
                 row = new HashMap<>();
-                for(int i = 1; i <= colCount; i++)
-                {
+                for (int i = 1; i <= colCount; i++) {
                     row.put(metaData.getColumnName(i), rs.getObject(i));
                 }
                 results.add(row);
             }
-       }
-       catch(Exception ex)
-       {
+        } catch (Exception ex) {
             System.out.println("A problem occurred while accessing records. "
                     + "Error message: " + ex.getMessage());
-       }
-       return results;
+        }
+        return results;
     }
-    
+
     /**
      * Accepts an SQL statement in a string and returns an integer, representing
      * the number of rows affected by the query. 0 indicates a query that
      * affects 0 rows
+     *
      * @param query delete, update, or insert
-     * @return 
+     * @return
      */
-    public int modifyRecords(String query)
-    {
+    public int modifyRecords(String query) {
         int rowsAffected = 0;
-        
-        try(Statement stmt = conn.createStatement())
-        {
+
+        try (Statement stmt = conn.createStatement()) {
             rowsAffected = stmt.executeUpdate(query);
-        } 
-       catch(Exception ex)
-        {
+        } catch (Exception ex) {
             System.out.println("A problem occurred while updating records. "
                     + "Error message: " + ex.getMessage());
         }
-       return rowsAffected; 
+        return rowsAffected;
     }
     /*End Charlotte's code*/
-    
+
     //**************************wayne*************************************8
     /**
      * Queries account table and returns to dashboard and printed in Textarea.
      *
      * @return
      */
-     public List<String> retrieveAccts(String query)
-    {
-       List<String> results = new ArrayList<>();
+    public List<String> retrieveAccts(String query) {
+        List<String> results = new ArrayList<>();
 
-       try (Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query))
-       {                  
-           rs.last();
-           int rowCount = rs.getRow();
-           rs.beforeFirst();
-           
-            for(int i = 1; i <= rowCount; i++)
-            {
+        try (Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(query)) {
+            rs.last();
+            int rowCount = rs.getRow();
+            rs.beforeFirst();
+
+            for (int i = 1; i <= rowCount; i++) {
                 results.add(rs.getString("account_name"));
             }
-       }
-       catch(Exception ex)
-       {
+        } catch (Exception ex) {
             System.out.println("A problem occurred while retrieving account details. "
                     + "Error message: " + ex.getMessage());
-       }
-       return results;
+        }
+        return results;
     }
-    
-    public int insertUser(String query)
-    {
+
+    public int insertUser(String query) {
         int rowsAffected = 0;
         Timestamp datetime = new Timestamp(new Date().getTime());
-        
-        try(PreparedStatement prepStmt = conn.prepareStatement(query))
-        {            
+
+        try (PreparedStatement prepStmt = conn.prepareStatement(query)) {
             prepStmt.setTimestamp(1, datetime);
             prepStmt.setTimestamp(2, datetime);
-            
+
             rowsAffected = prepStmt.executeUpdate();
-                     
-        } 
-       catch(Exception ex)
-        {
+
+        } catch (Exception ex) {
             System.out.println("A problem occurred while inserting the user record. "
                     + "Error message: " + ex.getMessage());
         }
-        
-       return rowsAffected; 
+
+        return rowsAffected;
     }
-    
-    public void closeConnection()
-    {
-        try
-        {
+
+    public void closeConnection() {
+        try {
             conn.close();
-        }
-        catch(Exception ex)
-        {
+        } catch (Exception ex) {
             System.out.println("There was a problem closing the connection to the database."
-                + "Error message: " + ex.getMessage());
+                    + "Error message: " + ex.getMessage());
         }
     }
 }
-
